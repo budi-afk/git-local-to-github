4a4d38ee6e84012ac67a9a72cc9dd0967e3acfef
budi-afk <budiman1347@gmail.com>
2022-02-16 09:56:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis illo aut, cupiditate culpa veniam itaque laudantium neque ad deleniti laborum eaque ipsam voluptas quasi architecto amet, alias at error delectus?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
